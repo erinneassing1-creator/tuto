616582bf5008f3514c6c141f4afd56c66bec57e1
erinneassing1 <erinneassing1@gmail.com>
2026-03-12 11:53:28 +0100

    Tuto 04

--- src/main/java/tuto/controller/ConnexionController.java
+++ src/main/java/tuto/controller/ConnexionController.java
@@ -3,13 +3,16 @@ package tuto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
+@SessionAttributes( "connected" )
 public class ConnexionController {
 
 	@GetMapping( "/" )
 	public String page1() {
-		return "connexion/home";
+		return "connexion/home.html";
 	}
 
 	@GetMapping( "/connect" )
@@ -17,11 +20,17 @@ public class ConnexionController {
 		var sb = new StringBuilder( username );
 		if ( sb.reverse().toString().equals(  password  ) ) {
 			model.addAttribute(  "connected", username  );
-			return "connexion/bonjour";
+			return "connexion/bonjour.html";
 		} else {
 			model.addAttribute( "alert", "Mot de passe incorrect" );
-			return "connexion/home";
+			return "connexion/home.html";
 		}
+	}
+	
+	@GetMapping( "/disconnect" )
+	public String disconnect(SessionStatus status) {
+		status.setComplete();
+		return "redirect:/";
 	}
 
 }
